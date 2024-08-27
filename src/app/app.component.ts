f424565fde91e8ca4bf6e11dc530979a05036168
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-08-27 23:08:55 +0200

    added intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { IntroComponent } from "./intro/intro.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { TestimonialsComponent } from "./testimonials/testimonials.component";
+import { ContactFormComponent } from "./contact-form/contact-form.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, IntroComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, TestimonialsComponent, ContactFormComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
