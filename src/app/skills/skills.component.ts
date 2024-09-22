dab44ec3c2d19bf70a947e4ed92b83a68971460c
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-09-22 21:00:21 +0200

    added skills section

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skills = [
+    { imgSrc: 'assets/skills-icon-angular.png', name: 'Angular' },
+    { imgSrc: 'assets/skills-icon-typescript.png', name: 'TypeScript' },
+    { imgSrc: 'assets/skills-icon-api.png', name: 'Rest-API' },
+    { imgSrc: 'assets/skills-icon-firebase.png', name: 'Firebase' },
+    { imgSrc: 'assets/skills-icon-git.png', name: 'Git' },
+    { imgSrc: 'assets/skills-icon-javascript.png', name: 'JavaScript' },
+    { imgSrc: 'assets/skills-icon-material-design.png', name: 'Material Design' },
+    { imgSrc: 'assets/skills-icon-scrum.png', name: 'Scrum' },
+    { imgSrc: 'assets/skills-icon-wordpress.png', name: 'WordPress' },
+    { imgSrc: 'assets/skills-icon-html.png', name: 'HTML' },
+    { imgSrc: 'assets/skills-icon-css.png', name: 'CSS' },
+    { imgSrc: 'assets/skills-icon-continually-learning-red.png', name: '' }
+  ];
 }
