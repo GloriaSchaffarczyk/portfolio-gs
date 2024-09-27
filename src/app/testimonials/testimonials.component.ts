2304d845a660865be50f2875a4bfc9e8a87cb967
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-09-27 23:06:59 +0200

    added quotation mark

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './testimonials.component.scss'
 })
 export class TestimonialsComponent {
-
+  testimonials = [
+    {
+      author: 'John',
+      quote: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      imageSrc: 'assets/testimonial-author01.png',
+    },
+    {
+      author: 'Marie',
+      quote: 'A simple Jump-and-Run game based on an object-oriented approach. Fight scary zombies and evil monsters with bombs while collecting Halloween candy.',
+      imageSrc: 'assets/testimonial-author01.png',
+    },
+    {
+      author: 'Bernd',
+      quote: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
+      imageSrc: 'assets/testimonial-author01.png',
+    },
+  ];
 }
