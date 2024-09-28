a4428d7b96b916e0e21c759b390d893671b664cb
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-09-28 11:57:12 +0200

    added slied - works half

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -5,24 +5,37 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './testimonials.component.html',
-  styleUrl: './testimonials.component.scss'
+  styleUrls: ['./testimonials.component.scss']
 })
 export class TestimonialsComponent {
   testimonials = [
-    {
-      author: 'John',
-      quote: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
-      imageSrc: 'assets/testimonial-author01.png',
-    },
-    {
-      author: 'Marie',
-      quote: 'A simple Jump-and-Run game based on an object-oriented approach. Fight scary zombies and evil monsters with bombs while collecting Halloween candy.',
-      imageSrc: 'assets/testimonial-author01.png',
-    },
-    {
-      author: 'Bernd',
-      quote: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
-      imageSrc: 'assets/testimonial-author01.png',
-    },
+    { author: 'John', quote: 'Task manager inspired by the Kanban System.', imageSrc: 'assets/testimonial-author01.png' },
+    { author: 'Marie', quote: 'A simple Jump-and-Run game.', imageSrc: 'assets/testimonial-author01.png' },
+    { author: 'Bernd', quote: 'Based on the PokéAPI.', imageSrc: 'assets/testimonial-author01.png' }
   ];
+
+  currentTestimonial = 0;
+
+  slideLeft() {
+    if (this.currentTestimonial === 0) {
+      this.currentTestimonial = this.testimonials.length - 1; // Zurück zum letzten Testimonial
+    } else {
+      this.currentTestimonial--;
+    }
+    this.updateSlidePosition();
+  }
+
+  slideRight() {
+    if (this.currentTestimonial === this.testimonials.length - 1) {
+      this.currentTestimonial = 0; // Zurück zum ersten Testimonial
+    } else {
+      this.currentTestimonial++;
+    }
+    this.updateSlidePosition();
+  }
+
+  updateSlidePosition() {
+    const testimonialWrapper = document.querySelector('.testimonial-wrapper') as HTMLElement;
+    testimonialWrapper.style.transform = `translateX(-${this.currentTestimonial * 100}%)`;
+  }
 }
