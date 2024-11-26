015134e700adefe220120fea89444bab749ad7a2
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-11-26 22:24:46 +0100

    improved contact section - validation

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -4,7 +4,7 @@ import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angula
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule], //eigentlich sollte hier das formsModule stehen
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
@@ -30,5 +30,44 @@ export class ContactFormComponent {
       ? '../../assets/home-arrow-green.png'
       : '../../assets/home-arrow-white.png';
   }
+
+  // Die Daten sollen in einem Object gespeichert werden
+  // contactData = {
+  //   name: "",
+  //   email: "",
+  //   message: "",
+  // }
+
+  // mailTest = true;
+
+  // post = {
+  //   endPoint: 'https://deineDomain.de/sendMail.php',
+  //   body: (payload: any) => JSON.stringify(payload),
+  //   options: {
+  //     headers: {
+  //       'Content-Type': 'text/plain',
+  //       responseType: 'text',
+  //     },
+  //   },
+  // };
+
+  // onSubmit(ngForm: NgForm) {
+  //   if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+  //     this.http.post(this.post.endPoint, this.post.body(this.contactData))
+  //       .subscribe({
+  //         next: (response) => {
+
+  //           ngForm.resetForm();
+  //         },
+  //         error: (error) => {
+  //           console.error(error);
+  //         },
+  //         complete: () => console.info('send post complete'),
+  //       });
+  //   } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+  //     ngForm.resetForm();
+  //   }
+  // }
   
 }
