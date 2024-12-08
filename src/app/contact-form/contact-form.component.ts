16bea19fdb94684a6431ced704324dd42b464635
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-12-08 19:13:51 +0100

    changed contact form to easier code and added validation for button

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,31 +1,29 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ReactiveFormsModule], //eigentlich sollte hier das formsModule stehen
+  imports: [FormsModule],
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent {
-
   http = inject(HttpClient);
-
   arrowSrc = '../../assets/home-arrow-white.png';
 
-  contactForm = new FormGroup({
-    name: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    message: new FormControl('', [Validators.required, Validators.minLength(10)]),
-    privacy: new FormControl(false, Validators.requiredTrue)
-  });
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+    privacy: false
+  };
 
-  onSubmit() {
-    if (this.contactForm.valid) {
-      console.log('Form Data:', this.contactForm.value);
-      // Hier kannst du die Daten verarbeiten (z.B. per HTTP senden)
+  onSubmit(contactForm: any) {
+    if (contactForm.valid) {
+      console.log('Form Data:', this.contactData);
+      contactForm.resetForm(); // Reset the form after submission
     }
   }
 
@@ -34,46 +32,4 @@ export class ContactFormComponent {
       ? '../../assets/home-arrow-green.png'
       : '../../assets/home-arrow-white.png';
   }
-
-  // neuer Code
-
-  // Die Daten sollen in einem Object gespeichert werden
-  // contactData = {
-  //   name: "",
-  //   email: "",
-  //   message: "",
-  // }
-
-  // mailTest = true;
-
-  // post = {
-  //   endPoint: 'https://deineDomain.de/sendMail.php',
-  //   body: (payload: any) => JSON.stringify(payload),
-  //   options: {
-  //     headers: {
-  //       'Content-Type': 'text/plain',
-  //       responseType: 'text',
-  //     },
-  //   },
-  // };
-
-  // onSubmit(ngForm: NgForm) {
-  //   if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-  //     this.http.post(this.post.endPoint, this.post.body(this.contactData))
-  //       .subscribe({
-  //         next: (response) => {
-
-  //           ngForm.resetForm();
-  //         },
-  //         error: (error) => {
-  //           console.error(error);
-  //         },
-  //         complete: () => console.info('send post complete'),
-  //       });
-  //   } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-
-  //     ngForm.resetForm();
-  //   }
-  // }
-  
 }
