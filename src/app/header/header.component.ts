06393f85d2ae324de37ef6f906b9e473220854d2
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-12-08 12:34:58 +0100

    added burger menu to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,10 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  mobileLinks = [
+    { text: 'About me', href: '#about-me' },
+    { text: 'Skills', href: '#skills' },
+    { text: 'Portfolio', href: '#portfolio' },
+    { text: 'Contact', href: '#contact' }
+  ];
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
 }
