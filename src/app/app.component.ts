3f1cd97af089e97051be1dfc2328eaf7c2baec03
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-12-11 12:25:33 +0100

    scss added padding and margins to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,30 +2,25 @@ import { Component, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-import { IntroComponent } from "./intro/intro.component";
-import { AboutMeComponent } from "./about-me/about-me.component";
-import { SkillsComponent } from "./skills/skills.component";
-import { PortfolioComponent } from "./portfolio/portfolio.component";
-import { TestimonialsComponent } from "./testimonials/testimonials.component";
-import { ContactFormComponent } from "./contact-form/contact-form.component";
 import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, IntroComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, TestimonialsComponent, ContactFormComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio-gs';
+
   constructor(private renderer: Renderer2) {}
 
   onMenuToggle(isMenuOpen: boolean): void {
     if (isMenuOpen) {
-      this.renderer.addClass(document.body, 'scrollStop'); // Scrollen deaktivieren
+      this.renderer.addClass(document.body, 'scrollStop');
     } else {
-      this.renderer.removeClass(document.body, 'scrollStop'); // Scrollen aktivieren
+      this.renderer.removeClass(document.body, 'scrollStop');
     }
   }
 }
