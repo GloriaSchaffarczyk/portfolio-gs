3396de7a443f70a691e5bf88f7f006737aa86da0
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-12-16 09:22:40 +0100

    added translations

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslationService } from '../translation.service';
 
@@ -7,8 +7,28 @@ import { TranslationService } from '../translation.service';
   standalone: true,
   imports: [RouterModule],
   templateUrl: './imprint.component.html',
-  styleUrls: ['./imprint.component.scss']
+  styleUrls: ['./imprint.component.scss'],
 })
-export class ImprintComponent {
-  constructor(public translationService: TranslationService) {}
+export class ImprintComponent implements AfterViewInit {
+  constructor(
+    public translationService: TranslationService, 
+    private el: ElementRef, 
+    private renderer: Renderer2 
+  ) {}
+
+  ngAfterViewInit(): void {
+    // Alle Links innerhalb der Komponente finden
+    const links: NodeListOf<HTMLAnchorElement> = this.el.nativeElement.querySelectorAll('a');
+  
+    // Sicherstellen, dass die Links durchlaufen werden können
+    Array.from(links).forEach((link) => {
+      const href = link.getAttribute('href');
+  
+      // Nur externe Links bearbeiten, die keine internen Navigationen (routerLink) sind
+      if (href && !href.startsWith('/') && !link.hasAttribute('routerLink')) {
+        this.renderer.setAttribute(link, 'target', '_blank');
+        this.renderer.setAttribute(link, 'rel', 'noopener noreferrer');
+      }
+    });
+  }  
 }
